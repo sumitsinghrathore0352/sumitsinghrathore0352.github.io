3431c6385a4757c615130d70f6f11e6f1ec17b72
sumitsinghrathore0325 <sumitsinghrajput0325@gmail.com>
2022-08-30 22:05:30 +0530

    check

--- cookstack/app.js
+++
@@ -1,7 +0,0 @@
-function toggle (){
-    let links = document.getElementById("links");
-    let mainHead =document.getElementById("mainHead");
-    mainHead.classList.toggle("homeHead");
-    links.classList.toggle("showNavigation");
-}
-
