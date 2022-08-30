ecb901ac29d81c21f2ce8dd6f8ceb05641553cb8
sumitsinghrathore0325 <sumitsinghrajput0325@gmail.com>
2022-08-30 22:02:35 +0530

    adding portfolio

---
+++ cookstack/app.js
@@ -0,0 +1,7 @@
+function toggle (){
+    let links = document.getElementById("links");
+    let mainHead =document.getElementById("mainHead");
+    mainHead.classList.toggle("homeHead");
+    links.classList.toggle("showNavigation");
+}
+
