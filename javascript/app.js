28040fd85fbfd6dd248dcb43262d8e9548fc09b3
sumitsinghrathore0325 <sumitsinghrajput0325@gmail.com>
2022-09-11 11:41:21 +0530

    skill toggle removed

--- javascript/app.js
+++ javascript/app.js
@@ -25,17 +25,11 @@ navLink.forEach((n) =>
   })
 );
 //   SKILLS
-function toggleSkills() {
-  const links = document.getElementById("skills_list-container");
-  links.classList.toggle("showSkills");
-  links.classList.toggle("skills_list-container");
-}
 // function toggleSkills() {
-//   const skillslist = document.querySelectorAll(".skills_list-container");
-//   console.log("skill list ", skillslist);
-//   for (var i = 0; i < skillslist.length; i++) {
-//     skillslist[i].classList.toggle("show-skills");
-//   }}
+//   const links = document.getElementById("skills_list-container");
+//   links.classList.toggle("showSkills");
+//   links.classList.toggle("skills_list-container");
+// }
 
 // SERVICES
 const modalViews = document.querySelectorAll(".services_modal"),
@@ -80,37 +74,3 @@ function scrollUp() {
   }
 }
 window.addEventListener("scroll", scrollUp);
-
-// // dark light theme
-
-// const themeButton = document.getElementById('theme-button');
-// const darkTheme = 'dark-theme';
-// const iconTheme = 'uil-sun';
-
-// //previously selected topic
-// const selectedTheme =  localStorage.getItem('selected-theme')
-// const selectedIcon =localStorage.getItem('selected-icon')
-// //we obtain the current theme that the interface has by validating the dark theme
-// const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light' ;
-// const getCurrentIcon = () => document.body.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun';
-// //we validate if user previsouly choose a topic
-// if(selectedTheme){
-//   // if the validation is fullfilled ,we ask thr issue was known if we activated or deactivated the dark theme
-
-//   if(selectedTheme === 'dark'){
-//     document.body.classList.add('dark-theme')
-//   }
-//   // document.body.classList[selectedTheme === 'dark' ? 'add': 'remove'](darkTheme);
-//   themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove' ] (iconTheme);
-//   console.log('x')
-// }
-// //activate the theme manually  with thw button
-// themeButton .addEventListener('click', () => {
-//   //add or remove the dark / icon theme
-//   document.body.classList.toggle('darkTheme');
-//   themeButton.classList.toggle('iconTheme');
-//   //we save the theme and the current icon that the user choose
-//   localStorage.setItem('selected-theme',getCurrentTheme())
-//   localStorage.setItem('selected-theme',getCurrentTheme())
-//   console.log('err')
-// })
